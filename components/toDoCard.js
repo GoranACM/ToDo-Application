8996512a945ca46c80ec7c769465e0bc134e9294
GoranACM <goranacm@gmail.com>
2020-08-22 17:17:43 +1000

    Added checkbox to mard task as done

--- components/toDoCard.js
+++ components/toDoCard.js
@@ -3,32 +3,35 @@
 // Academy of Information Technology
 // 2020
 
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, CheckBox } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 // Install react-native-elements
 // import { CheckBox } from 'react-native-elements'
 
 export default function ToDoCard({ item, pressHandler }) {
+    const [isSelected, setSelection] = useState(false);
 
     return (
         <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <View style={styles.item}>
-
-            
-            {/* <CheckBox
-                center
-                title='Click Here to Remove This Item'
-                iconRight
-                iconType='material'
-                checkedIcon='clear'
-                uncheckedIcon='add'
-                checkedColor='red'
-                
-            /> */}
-                <MaterialIcons name="delete" size={18} color='#333'/>
-                <Text style={styles.itemText}>{item.text}</Text>
+            <View style={[
+                styles.item,
+                {backgroundColor: isSelected ? "#11a9b0" : "white"}
+            ]}>
+                <View style={styles.checkboxContainer}>
+                    <CheckBox
+                        value={isSelected}
+                        onValueChange={setSelection}
+                        style={styles.checkbox}               
+                    /> 
+                </View>
+                {/* <MaterialIcons name="delete" size={18} color='#333'/> */}
+                <Text 
+                    style={[
+                        {textDecorationLine: isSelected ? "line-through" : "none"},
+                        styles.itemText,
+                    ]}>{item.text}</Text>
             </View>
         </TouchableOpacity>
     )
@@ -40,13 +43,27 @@ const styles = StyleSheet.create({
         padding: 16,
         marginTop: 16,
         borderColor: '#bbb',
-        borderWidth: 1,
-        borderStyle: 'dashed',
+        borderWidth: 2,
+        borderStyle: 'solid',
         borderColor: '#11a9b0',
         borderRadius: 10,
     },
 
     itemText: {
+        fontSize: 22,
         marginLeft: 10,
-    }
+    },
+
+    itemTextCrossed: {
+        textDecorationLine: "line-through",
+    },
+
+    checkboxContainer: {
+        flexDirection: "row",
+        
+    },
+
+    checkbox: {
+        alignSelf: "center",
+    },
 });
